b9902a647dfacaef91d78bc366d98fdda25c20ed
Jessie Xu <jessieunc@gmail.com>
2016-02-09 17:07:59 -0800

    Update Social-Network Query Exercise.sql

--- Social-Network Query Exercise.sql
+++ Social-Network Query Exercise.sql
@@ -31,3 +31,34 @@ where h1.name < h2.name and id1 in
     (select l1.id1 from likes l1
        join likes l2 on l1.id1 = l2.id2 and l1.id2 = l2.id1) 
 order by h1.name, h2.name;
+
+-- Q4. Find all students who do not appear in the Likes table (as a student who likes or is liked) 
+-- and return their names and grades. Sort by grade, then by name within each grade.
+select name, grade
+  from highschooler
+where id not in 
+    (select id1 from likes 
+       union 
+     select id2 from likes)
+order by grade, name;
+
+-- Q5. For every situation where student A likes student B, but we have no information about whom B likes 
+-- (that is, B does not appear as an ID1 in the Likes table), return A and B's names and grades.
+select h1.name as name, h1.grade as grade, h2.name as like_name, h2.grade as like_grade
+  from highschooler h1
+  join likes on h1.id = id1
+  join highschooler h2 on h2.id = id2
+where id2 not in (select id1 from likes);
+
+-- Q6. Find names and grades of students who only have friends in the same grade. 
+-- Return the result sorted by grade, then by name within each grade.
+
+select name, grade
+  from highschooler
+where id not in 
+    (select id1
+       from highschooler h1
+       join friend on h1.id = id1
+       join highschooler h2 on h2.id = id2
+     where h1.grade != h2.grade)
+     order by grade, name;
